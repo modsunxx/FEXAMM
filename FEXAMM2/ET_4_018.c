8986f9a82c10a8c69a39e2dcec3966736879b991
adipa choupolsat <modsunxxu@example.com>
2024-10-24 21:35:55 +0700

    c-code

--- FEXAMM2/ET_4_018.c
+++ FEXAMM2/ET_4_018.c
@@ -3,18 +3,18 @@
 int main() {
     int size;
     printf("Input the size of array: ");
-    scanf("%d", &size);
-    int array[size];
+    scanf("%d", &size);//รับค่าตัวแปรมาเก็บในตัวแปรsize
+    int array[size];//สร้างตัวแปรarrayให้มีขนาดเท่ากับsizeที่รับเข้ามา
     printf("Input %d elements in the array:\n", size);
 
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) {//วนลูปเพื่อที่จะรับค่าเข้ามาเก็บในตัวแปรarray
         printf("element - %d: ", i + 1);
         scanf("%d", &array[i]); 
     }
 
-    int min = array[0], second_min = array[0];
+    int min = array[0], second_min = array[0];//ประกาศตัวแปรน้อยสุดกับน้อยอันดับสองให้เป็น0
 
-    for (int i = 1; i < size; i++) {
+    for (int i = 1; i < size; i++) {//วนลูปหาจำนวนน้อยสุด
         if (array[i] < min) {
             second_min = min;
             min = array[i];
@@ -23,7 +23,7 @@ int main() {
         }
     }
 
-    if (second_min == min) { 
+    if (second_min == min) { //ถ้าหากรองน้อยสุดเท่ากับน้อยสุดกำหนดค่าให้รองน้อยสุดมีค่าเป็น1
         second_min = array[1];
         for (int i = 0; i < size; i++) {
             if (array[i] != min) {
@@ -31,7 +31,7 @@ int main() {
                 break;
             }
         }
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {//วนลูปหาค่าที่น้อยเป็นอันดับสอง
             if (array[i] < second_min && array[i] != min) {
                 second_min = array[i];
             }
